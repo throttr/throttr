58da09038eaab2952dab75a53f635f631aa1c0eb
Ian Torres <iantorres@outlook.com>
2025-04-26 01:30:04 -0400

    Fix.

--- src/include/throttr/request.hpp
+++ src/include/throttr/request.hpp
@@ -70,9 +70,7 @@ namespace throttr {
         std::size_t operator()(const request_key &key) const {
             std::size_t _h = std::hash<uint8_t>{}(key.ip_version_);
 
-            for (const auto _b: key.ip_) {
-                _h ^= std::hash<uint8_t>{}(_b) + 0x9e3779b9 + (_h << 6) + (_h >> 2);
-            }
+            for (const auto _b: key.ip_) _h ^= std::hash<uint8_t>{}(_b) + 0x9e3779b9 + (_h << 6) + (_h >> 2);
 
             _h ^= std::hash<uint16_t>{}(key.port_) + 0x9e3779b9 + (_h << 6) + (_h >> 2);
             _h ^= std::hash<std::string>{}(key.url_) + 0x9e3779b9 + (_h << 6) + (_h >> 2);
